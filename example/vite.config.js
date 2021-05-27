a90202937fd73d3083216078c7424d1c3021771e
ahwgs <ah_wgs@126.com>
2021-05-27 22:45:09 +0800

    feat: 修改打包方式

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,47 +1,47 @@
-import htmlPlugin from '../dist/index';
+import htmlPlugin from "../dist/index.esm";
 
 const htmlPluginOpt = {
-  favicon: './logo.svg',
+  favicon: "./logo.svg",
   headScripts: [
     `var msg = 'head script'
      console.log(msg);`,
     {
       async: true,
-      src: 'https://abc.com/b.js',
-      type: 'module',
+      src: "https://abc.com/b.js",
+      type: "module",
     },
-    { content: `console.log('hello')`, charset: 'utf-8' },
+    { content: `console.log('hello')`, charset: "utf-8" },
   ],
   scripts: [
     `var msg = 'body script'
      console.log(msg);`,
     {
       async: true,
-      src: 'https://abc.com/b.js',
-      type: 'module',
+      src: "https://abc.com/b.js",
+      type: "module",
     },
   ],
   metas: [
     {
-      name: 'keywords',
-      content: 'vite html meta keywords',
+      name: "keywords",
+      content: "vite html meta keywords",
     },
     {
-      name: 'description',
-      content: 'vite html meta description',
+      name: "description",
+      content: "vite html meta description",
     },
     {
-      bar: 'custom meta',
+      bar: "custom meta",
     },
   ],
   links: [
     {
-      rel: 'stylesheet',
-      href: './style.css',
+      rel: "stylesheet",
+      href: "./style.css",
     },
     {
-      rel: 'modulepreload',
-      href: 'https://cn.vitejs.dev/assets/guide_api-plugin.md.6884005a.lean.js',
+      rel: "modulepreload",
+      href: "https://cn.vitejs.dev/assets/guide_api-plugin.md.6884005a.lean.js",
     },
   ],
   style: `body { color: red; };*{ margin: 0px }`,
