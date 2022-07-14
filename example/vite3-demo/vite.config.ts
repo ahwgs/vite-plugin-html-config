1eeece560ee181c4050006cd2ff02dbb12847f9e
ahwgs <ah_wgs@126.com>
2022-07-14 22:44:38 +0800

    feat: 支持vite3

---
+++ example/vite3-demo/vite.config.ts
@@ -0,0 +1,54 @@
+import htmlPlugin from "vite-plugin-html-config";
+
+console.log("htmlPlugin", htmlPlugin);
+
+module.exports = {
+  plugins: [
+    htmlPlugin({
+      favicon: "./logo.svg",
+      headScripts: [
+        `var msg = 'head script'
+     console.log(msg);`,
+        {
+          async: true,
+          src: "https://abc.com/b.js",
+          type: "module",
+        },
+        { content: `console.log('hello')`, charset: "utf-8" },
+      ],
+      scripts: [
+        `var msg = 'body script'
+     console.log(msg);`,
+        {
+          async: true,
+          src: "https://abc.com/b.js",
+          type: "module",
+        },
+      ],
+      metas: [
+        {
+          name: "keywords",
+          content: "vite html meta keywords",
+        },
+        {
+          name: "description",
+          content: "vite html meta description",
+        },
+        {
+          bar: "custom meta",
+        },
+      ],
+      links: [
+        {
+          rel: "stylesheet",
+          href: "./style.css",
+        },
+        {
+          rel: "modulepreload",
+          href: "https://cn.vitejs.dev/assets/guide_api-plugin.md.6884005a.lean.js",
+        },
+      ],
+      style: `body { color: #fff; };*{ margin: 0px }`,
+    }),
+  ],
+};
